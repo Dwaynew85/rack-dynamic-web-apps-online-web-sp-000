a8a6e9379d4175582782cd9598737c8d83459e3f
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-21 07:19:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,9 +7,9 @@ class Application
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
 
-    if num_1==num_2 && num_2==num_3 
+    if num_1==num_2 && num_2==num_3
       resp.write "You Win"
-    else 
+    else
       resp.write "You Lose"
     end
 
